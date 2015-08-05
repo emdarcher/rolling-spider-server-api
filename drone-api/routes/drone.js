038cbc86f2abab9b4189286423db05605ec4e64b
Ethan Durrant <ethan.durrant@mulesoft.com>
2015-08-05 16:33:54 -0700

    added all other control functions

--- drone-api/routes/drone.js
+++ drone-api/routes/drone.js
@@ -156,7 +156,67 @@ exports.droneEmergency = function(req, res, next) {
 
 /*
  * cmdDetails = {
- *  cmd_func: <rs_func>,
+ *  cmd_func: function(cb){ rs_func(cb); },
+ *  cmd_name: <funcname>,
+ * };
+ */
+function singleCmd(req, res, next, cmdDetails) {
+    //executes single command without options
+    console.log('calling singleCmd with ' + cmdDetails.cmd_name);
+    console.log('sending ' + cmdDetails.cmd_name + ' command');
+    cmdDetails.cmd_func(function() {
+        console.log('drone executed ' + cmdDetails.cmd_name + ' command');
+        return res.json({"message":"drone executed an amazing "
+                                + cmdDetails.cmd_name });
+    });
+    next();
+
+}
+
+exports.droneFrontFlip = function(req, res, next) {
+    console.log('called path to frontFlip function');
+    var details = {
+        cmd_func: function(cb){ rollingSpider.frontFlip(cb); },
+        cmd_name: 'frontFlip'
+    };
+    singleCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneBackFlip = function(req, res, next) {
+    console.log('called path to backFlip function');
+    var details = {
+        cmd_func: function(cb){ rollingSpider.backFlip(cb); },
+        cmd_name: 'backFlip'
+    };
+    singleCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneLeftFlip = function(req, res, next) {
+    console.log('called path to leftFlip function');
+    var details = {
+        cmd_func: function(cb){ rollingSpider.leftFlip(cb); },
+        cmd_name: 'leftFlip'
+    };
+    singleCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneRightFlip = function(req, res, next) {
+    console.log('called path to rightFlip function');
+    var details = {
+        cmd_func: function(cb){ rollingSpider.rightFlip(cb); },
+        cmd_name: 'rightFlip'
+    };
+    singleCmd(req, res, next, details);
+    return next();
+};
+
+
+/*
+ * cmdDetails = {
+ *  cmd_func: function(opt,cb){ rs_func(opt,cb); },
  *  cmd_name: <funcname>,
  *  options: {
  *      speed: <speed>,
@@ -165,14 +225,14 @@ exports.droneEmergency = function(req, res, next) {
  * };
  * */
 function driveCmd(req, res, next, cmdDetails) {
-    //executes a drive type command
-    var error = false;
+    //executes a drive type command with options
     console.log('calling driveCmd with ' + cmdDetails.cmd_name);
     Object.keys(cmdDetails.options).forEach(function(key){
         var item = cmdDetails.options[key];
         if( typeof item !== "number" ){
             console.log(item + ' is not a number!');
-            return res.json(400, {"error": item + " is not a number!"});
+            return res.json(400, {"error": "\"" + item + "\"" 
+                                    + " is not a number!"});
         } else if (!((item >= 0) && (item <= 100))){
             console.log(item + " is not in range 0-100!");
             return res.json(400, {"error": item + " is not in range 0-100!"});  
@@ -194,9 +254,134 @@ exports.droneForward = function(req, res, next) {
     //var json_options = req.params;
     console.log('called path to forward function');
     var details = {
-        //cmd_func: rollingSpider.forward,
         cmd_func: function(opt, cb){ rollingSpider.forward(opt,cb); },
         cmd_name: "forward",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneBackward = function(req, res, next) {
+    console.log('called path to backward function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.backward(opt,cb); },
+        cmd_name: "backward",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneUp = function(req, res, next) {
+    console.log('called path to up function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.up(opt,cb); },
+        cmd_name: "up",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneDown = function(req, res, next) {
+    console.log('called path to down function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.down(opt,cb); },
+        cmd_name: "down",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneLeft = function(req, res, next) {
+    console.log('called path to left function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.left(opt,cb); },
+        cmd_name: "left",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneRight = function(req, res, next) {
+    console.log('called path to right function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.right(opt,cb); },
+        cmd_name: "right",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneTiltLeft = function(req, res, next) {
+    console.log('called path to tiltLeft function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.tiltLeft(opt,cb); },
+        cmd_name: "tiltLeft",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneTiltRight = function(req, res, next) {
+    console.log('called path to tiltRight function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.tiltRight(opt,cb); },
+        cmd_name: "tiltRight",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneTurnLeft = function(req, res, next) {
+    console.log('called path to turnLeft function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.turnLeft(opt,cb); },
+        cmd_name: "turnLeft",
+        options: {
+            speed: req.params.speed,
+            steps: req.params.steps 
+        }
+    }; 
+    driveCmd(req, res, next, details);
+    return next();
+};
+
+exports.droneTurnRight = function(req, res, next) {
+    console.log('called path to turnRight function');
+    var details = {
+        cmd_func: function(opt, cb){ rollingSpider.turnRight(opt,cb); },
+        cmd_name: "turnRight",
         options: {
             speed: req.params.speed,
             steps: req.params.steps 
--- drone-api/server.js
+++ drone-api/server.js
@@ -1,5 +1,4 @@
 //server
-//
 
 var restify = require('restify');
 
@@ -49,11 +48,24 @@ server.get(prefix_path + '/', function(req,res,next){
 
 // routes directed to functions in /routes/drone.js
 server.get(prefix_path + '/drone/', drone.getDroneData ); 
-server.post(prefix_path + '/drone/calibrate/', drone.droneCalibrate ); 
-server.post(prefix_path + '/drone/takeOff/', drone.droneTakeOff ); 
-server.post(prefix_path + '/drone/land/', drone.droneLand ); 
-server.post(prefix_path + '/drone/emergency/', drone.droneEmergency ); 
-server.post(prefix_path + '/drone/forward/', drone.droneForward ); 
+server.post(prefix_path + '/drone/calibrate/',  drone.droneCalibrate ); 
+server.post(prefix_path + '/drone/takeOff/',    drone.droneTakeOff ); 
+server.post(prefix_path + '/drone/land/',       drone.droneLand ); 
+server.post(prefix_path + '/drone/emergency/',  drone.droneEmergency ); 
+server.post(prefix_path + '/drone/forward/',    drone.droneForward ); 
+server.post(prefix_path + '/drone/backward/',   drone.droneBackward ); 
+server.post(prefix_path + '/drone/up/',         drone.droneUp ); 
+server.post(prefix_path + '/drone/down/',       drone.droneDown ); 
+server.post(prefix_path + '/drone/right/',      drone.droneRight ); 
+server.post(prefix_path + '/drone/left/',       drone.droneLeft ); 
+server.post(prefix_path + '/drone/tiltRight/',  drone.droneTiltRight ); 
+server.post(prefix_path + '/drone/tiltLeft/',   drone.droneTiltLeft ); 
+server.post(prefix_path + '/drone/turnRight/',  drone.droneTurnRight ); 
+server.post(prefix_path + '/drone/turnLeft/',   drone.droneTurnLeft ); 
+server.post(prefix_path + '/drone/frontFlip/',  drone.droneFrontFlip ); 
+server.post(prefix_path + '/drone/backFlip/',   drone.droneBackFlip ); 
+server.post(prefix_path + '/drone/rightFlip/',  drone.droneRightFlip ); 
+server.post(prefix_path + '/drone/leftFlip/',   drone.droneLeftFlip ); 
 
 
 // start the server
